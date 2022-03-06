7fb4fb600129b8e048602b796858eeafe736c7b0
Baby Blue <luke.mcfarland@babyblue.info>
2022-03-06 21:15:12 +0000

    Update

--- javascript/my.js
+++ javascript/my.js
@@ -1,11 +1,26 @@
-$(function () {
-    $('.genealogy-tree ul').hide();
-    $('.genealogy-tree>ul').show();
-    $('.genealogy-tree ul.active').show();
-    $('.genealogy-tree li').on('click', function (e) {
-        var children = $(this).find('> ul');
-        if (children.is(":visible")) children.hide('fast').removeClass('active');
-        else children.show('fast').addClass('active');
-        e.stopPropagation();
+$(document).ready(function() {
+
+    function toggleSidebar() {
+      $(".cart_button").toggleClass("active");
+      $("main").toggleClass("move-to-left");
+      $(".sidebar-item").toggleClass("active");
+    }
+  
+    $(".cart_button").on("click tap", function() {
+      toggleSidebar();
     });
+  
+    $(document).keyup(function(e) {
+      if (e.keyCode === 27) {
+        toggleSidebar();
+      }
+    });
+  
+  });
+
+
+  $(document).ready(function(){
+  $(".cart_button").click(function(){
+    $("body").toggleClass("transperent");
+  });
 });
