c5b6e5470a0d06815526c92f401181bf4cb64fac
Baby Blue <luke.mcfarland@babyblue.info>
2022-03-06 21:40:04 +0000

    Reusable Header Added

--- javascript/my.js
+++ javascript/my.js
@@ -1,26 +1,43 @@
 $(document).ready(function() {
 
-    function toggleSidebar() {
-      $(".cart_button").toggleClass("active");
-      $("main").toggleClass("move-to-left");
-      $(".sidebar-item").toggleClass("active");
-    }
-  
-    $(".cart_button").on("click tap", function() {
-      toggleSidebar();
-    });
-  
-    $(document).keyup(function(e) {
-      if (e.keyCode === 27) {
-        toggleSidebar();
-      }
-    });
-  
-  });
+function toggleSidebar() {
+    $(".cart_button").toggleClass("active");
+    $("main").toggleClass("move-to-left");
+    $(".sidebar-item").toggleClass("active");
+}
 
-
-  $(document).ready(function(){
-  $(".cart_button").click(function(){
-    $("body").toggleClass("transperent");
-  });
+$(".cart_button").on("click tap", function() {
+    toggleSidebar();
 });
+
+$(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+    toggleSidebar();
+    }
+});
+
+});
+
+
+$(document).ready(function(){
+    $(".cart_button").click(function(){
+    $("body").toggleClass("transperent");
+    });
+});
+
+class Header extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+            <header>
+
+            <div  class="header_menu">
+                <img src="images/logo.svg" alt="logo" id="header__logo">
+                <img src="images/bag 2.png" alt="bag-2" id="header__bag">
+            </div>
+
+            </header>
+        `
+    }
+  }
+
+  customElements.define('main-header', Header);
